ab08daa290be28c68868c18a8584b62626b0fa6a
Isaias Pomales <impomales@gmail.com>
2019-01-09 15:49:27 -0500

    set up basic front end form

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res, next) => {
-    res.send('hello')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
